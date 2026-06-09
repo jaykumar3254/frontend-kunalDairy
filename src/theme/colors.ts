d8bd4780af01cb90c0b5c7493b2d0bbb85dc6ecb
JayKumar <pardeshijaikumar@gmail.com>
2026-06-09 20:07:53 +0530

    boiler for files

---
+++ src/theme/colors.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  primary: "#2563EB",
+  secondary: "#10B981",
+
+  background: "#F8FAFC",
+  white: "#FFFFFF",
+
+  text: "#111827",
+  gray: "#6B7280",
+
+  success: "#22C55E",
+  warning: "#F59E0B",
+  danger: "#EF4444",
+};
